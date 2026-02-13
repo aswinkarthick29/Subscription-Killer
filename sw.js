1c7b1539d8209e43e150e895fe6f598cabe4ea60
aswinkarthick29 <akaswinkarthick@gmail.com>
2026-02-13 22:17:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,4 +9,13 @@ self.addEventListener('notificationclick', function(event) {
 // Add this to your sw.js file
 self.addEventListener('fetch', function(event) {
     // This can be empty, but it MUST exist for the install button to show up
+
+});
+// This is the minimum code required for the "Install" button to appear
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', (event) => {
+    // This allows the app to be installable
 });
